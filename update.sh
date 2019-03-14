6011de665c736af1525ca9e7247be668c07376d0
MastaG <mastag@gmail.com>
2019-03-14 11:25:21 +0100

    Wayland and samba are now fixed to build with lld

--- update.sh
+++ update.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-for i in bitbake meta-browser meta-clang meta-odroid meta-openembedded meta-qt5 meta-rust openembedded-core
+git pull
+for i in bitbake meta-browser meta-odroid meta-openembedded meta-qt5 meta-rust openembedded-core
 do
 	echo "Updating ${i}"
 	cd ${i}
 	git checkout master
 	git pull --all
 	cd ..
+	git add ${i}
 done
-git add bitbake meta-browser meta-clang meta-odroid meta-openembedded meta-qt5 meta-rust openembedded-core
 git commit -m "Update all submodule"
+cd meta-clang
+git fetch upstream
+git merge --commit upstream/master
