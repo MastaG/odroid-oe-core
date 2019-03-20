fd9d843bedc298aba03e7b1815b7a5df5b30aa83
MastaG <mastag@gmail.com>
2019-03-20 13:30:16 +0100

    Modify update.sh for sumo

--- update.sh
+++ update.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 git pull
-for i in bitbake meta-clang meta-browser meta-odroid meta-openembedded meta-qt5 meta-rust openembedded-core
+for i in meta-clang meta-odroid meta-openembedded meta-qt5 meta-rust openembedded-core
 do
 	echo "Updating ${i}"
+	cd ${i}
+	git checkout sumo
+	git pull --all
+	cd ..
+	git add ${i}
+done
+for i in bitbake meta-browser
+do
+	echo echo "Updating ${i}"
 	cd ${i}
 	git checkout master
 	git pull --all
 	cd ..
 	git add ${i}
 done
-git commit -m "Update all submodule"
+git commit -m "Update all submodules"
